51221152e2f3e2f90fa704ba4c70a5d2c646ebbc
Ret Samys <retsamys@gmx.net>
2021-05-19 13:58:32 +0200

    Update data.php

--- data.php
+++ data.php
@@ -1,3 +1,46 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$out="";
+//echo "var tars=";
+$out.= "var tars=";
+
+$path=$_SERVER["DOCUMENT_ROOT"]."/protected/mapdata.csv";
+$csv = array_map('str_getcsv', file($path));
+$output=[];
+$books=explode("|",file_get_contents("books.txt"));
+
+foreach($csv as $line){
+	if ($line[0]=="1"){
+		$title=$line[4];
+		if ($line[5]=="" or $line[5]=="not found"){}else{
+			$title=$line[5].", ".$title;
+		}
+		if ($line[6]=="" or $line[6]=="not found"){}else{
+			$title=$line[6]." (".$title.")";
+		}
+		if($title=="" or $title=="not found"){
+			$title="unknown";
+		}
+		if (array_key_exists($line[1],$output)){
+			array_push($output[$line[1]],[$line[2],$line[3],"<b>".$title."</b>".(strlen($line[8])>0?"<br><b>From:</b> ".$line[8]:"")."<br><img src='photos/".$line[7]."'><br>".$line[10].(strlen($line[9])>0?"<br><b>Date of arrival:</b> ".$line[9]:"").($line[11]>=0?"<br><b>Rating of this location:</b> ".($line[11]/2)." out of 5 stars":""),$title]);
+		}else{
+			$output[$line[1]]=[[$line[2],$line[3],"<b>".$title."</b><br><b>From:</b> ".$line[8]."<br><img src='photos/".$line[7]."'><br>".$line[10].(strlen($line[9])>0?"<br><b>Date of arrival:</b> ".$line[9]:"").($line[11]>=0?"<br><b>Rating of this location:</b> ".($line[11]/2)." out of 5 stars":""),$title]];
+		}
+	}
+}
+
+//echo json_encode($output,JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+$out.=json_encode($output,JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+//echo ";";
+$out.=";";
+$out.="var allbooks=".json_encode($books,JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+file_put_contents("data.js",$out);
+
+
+/*
+
 var tars=<?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -27,4 +70,8 @@ foreach($csv as $line){
 }
 
 echo json_encode($output,JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-?>;
+
+
+*/
+
+?>
